e2521b2d25e2685618df5c11452a43ea50d0b5d0
Melonchanism <121840186+Melonchanism@users.noreply.github.com>
2026-06-25 16:15:42 -0700

    refactor: use main.swift to skip SwiftUI App initialization

---
+++ Barik/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  Barik
+//
+//  Created by josh on 6/25/26.
+//
+import AppKit
+
+let delegate = AppDelegate()
+
+let app = NSApplication.shared
+
+app.delegate = delegate
+app.run()
